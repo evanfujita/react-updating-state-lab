b5fdea4643e992a4f94c6fa9a6d79c247f43fa2b
Evan <evanfujita@gmail.com>
2021-02-24 12:54:20 -0600

    commit tree 2828ba18d892a12ea415d80facdc6dae0082f93b

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,34 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+
+    }
+
+
+    render(){
+
+        const addClick = () => {
+            const plusClick = this.state.timesClicked + 1
+            this.setState({
+                timesClicked: plusClick
+            })
+        }
+
+        return(
+            <div>
+                <button onClick={addClick} >{this.state.timesClicked}</button>
+            </div>
+
+        )
+
+    }
+
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,64 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+
+
+
+
+    render(){
+        const handleBitrate = () => {
+            this.setState({
+                errors: this.state.errors,
+                user: this.state.user,
+                    settings: {
+                        bitrate: 12,
+                        video: {
+                            resolution: this.state.settings.video.resolution
+                    }
+                }
+
+            })
+        }
+
+        const handleResolution = () => {
+            this.setState({
+                errors: this.state.errors,
+                user: this.state.user,
+                    settings: {
+                        bitrate: this.state.settings.bitrate,
+                        video: {
+                            resolution: '720p'
+                    }
+                }
+
+            })
+        }
+
+
+        return(
+            <div>
+                <button className='bitrate' onClick={handleBitrate}>{this.state.settings.bitrate} bitrate</button>
+                <button className='resolution' onClick={handleResolution}>{this.state.settings.video.resolution} resolution</button>
+            </div>
+        )
+
+    }
+
+
+}
+
+export default YouTubeDebugger
